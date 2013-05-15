96752b39306ddaabc9981f9e7bb422fab0e32532
Hong Xu <hong@topbug.net>
2013-05-15 11:27:18 -0700

    Bump version number to 0.1.2 and update Gemfile.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
+gemspec
 gem 'rake', '~> 10.0.4'
 gem 'rspec', '~> 2.13.0'
-gem 'trollop', '~> 2.0'
--- lib/skm/version.rb
+++ lib/skm/version.rb
@@ -4,7 +4,7 @@ module SshKeyManager #:nodoc:#
   module VERSION
     MAJOR = 0
     MINOR = 1
-    TINY  = 1
+    TINY  = 2
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
   end
